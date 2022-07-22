b14ba89d2e130dfaf06e9fef7e43a7b3ec62fb94
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2022-07-23 00:38:15 +0900

    first commit

---
+++ src/contexts/ColorModeContext.tsx
@@ -0,0 +1,40 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import React, { createContext, ReactNode, useMemo, useState } from "react";
+
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+export default function ColorModeProvider(props: {
+  children:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}) {
+  const [mode, setMode] = useState<"light" | "dark">("light");
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: { mode },
+      }),
+    [mode]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
